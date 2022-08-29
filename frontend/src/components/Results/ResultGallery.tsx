dba524c07f8546715c79074104cf174b52e9bf84
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-08-29 15:12:36 +0200

    filters added

--- frontend/src/components/Results/ResultGallery.tsx
+++ frontend/src/components/Results/ResultGallery.tsx
@@ -4,6 +4,7 @@ import {AxiosError} from "axios";
 import {useAuth} from "../../auth/AuthProvider";
 import {ValidatedAnswers} from "../../service/models";
 import ResultCard from "./ResultCard";
+import ResultFilter from "./ResultFilter";
 
 interface ResultGalleryProps{
     id : string
@@ -12,8 +13,16 @@ interface ResultGalleryProps{
 export default function ResultGallery({id}:ResultGalleryProps){
 
     const [results,setResults]=useState<ValidatedAnswers>()
+    const [filterCorrect, setFilterCorrect] = useState('alle')
+    const [filterCategory, setFilterCategory] = useState('alle')
 
     const {token} = useAuth()
+
+    const filterCorrectValue = ['alle','richtig','falsch']
+    const filterCorrectTexts = ['Alle', 'Nur Richtige','Nur Falsche']
+
+    const filterCategoryValue = ['alle','SECURITY','CLOUD','BILLING','TECHNOLOGY']
+    const filterCategoryTexts = ['Alle','Security and Compliance', 'Cloud Concepts','Billing and Pricing','Technology']
 
     useEffect(()=>{
         if(id&&token){
@@ -32,9 +41,28 @@ export default function ResultGallery({id}:ResultGalleryProps){
             {
                 results ?
                     <div>
+                        <ResultFilter selected={filterCorrect} handleChange={setFilterCorrect} options={filterCorrectValue} texts={filterCorrectTexts}/>
+                        <ResultFilter selected={filterCategory} handleChange={setFilterCategory} options={filterCategoryValue} texts={filterCategoryTexts}/>
                         {
-                            results.takenQuestions.map((e,i) => {
-                                return <ResultCard key={i} result={e} index={i}/>
+                            results.takenQuestions
+                                .filter(e => {
+                                    if (filterCorrect!=='alle'){
+                                        if (filterCorrect==='richtig'){
+                                            return !e.correctlyAnswers.includes(false)
+                                        } else {
+                                            return e.correctlyAnswers.includes(false)
+                                        }
+                                    }
+                                    return e
+                                })
+                                .filter(e =>{
+                                    if (filterCategory!=='alle'){
+                                        return e.question.category===filterCategory
+                                    }
+                                    return e
+                                })
+                                .map((e,i) => {
+                                return <ResultCard key={e.question.question} result={e} index={i}/>
                             })
                         }
                     </div>
--- frontend/src/service/data.ts
+++ frontend/src/service/data.ts
@@ -104,3 +104,6 @@ export const multiAnswerChoice = [
     }
 ]
 
+//Filter
+
+
